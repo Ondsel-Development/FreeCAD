624d5852e27448506ce1ebd9b61f42ea31ac6ac3
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2024-09-18 18:30:07 -0400

    Start: change command icon to home and alos set it as StartView icon

--- src/Mod/Start/Gui/StartView.cpp
+++ src/Mod/Start/Gui/StartView.cpp
@@ -47,6 +47,7 @@
 #include <Base/Interpreter.h>
 #include <Base/Tools.h>
 #include <Gui/Application.h>
+#include <Gui/BitmapFactory.h>
 #include <Gui/Command.h>
 #include <Gui/MainWindow.h>
 #include <Gui/Document.h>
@@ -116,6 +117,7 @@ StartView::StartView(QWidget* parent)
     , _showOnStartupCheckBox {nullptr}
 {
     setObjectName(QLatin1String("StartView"));
+    setWindowIcon(Gui::BitmapFactory().pixmap("StartCommandIcon"));
     auto hGrp = App::GetApplication().GetParameterGroupByPath(
         "User parameter:BaseApp/Preferences/Mod/Start");
     auto cardSpacing = hGrp->GetInt("FileCardSpacing", 15);  // NOLINT
